4af9cd28053b932c226374034b60b82e2abfa767
nrcsantos <nrcsantos@me.com>
2018-01-21 11:24:51 +0100

    modified:   resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Nothing to see here...
                 </div>
 
                 <div class="links">
